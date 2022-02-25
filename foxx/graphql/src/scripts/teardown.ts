a8d9d584b5d69cb744ac0132b6276ceee07c98bf
Manuel Ruck <git@manuelruck.de>
2022-02-25 15:10:23 +0100

    remove unused foxx service

--- foxx/graphql/src/scripts/teardown.ts
+++
@@ -1,6 +0,0 @@
-import { db } from "@arangodb";
-
-db._drop(module.context.collectionName("episodes"));
-db._drop(module.context.collectionName("characters"));
-db._drop(module.context.collectionName("friends"));
-db._drop(module.context.collectionName("appearsIn"));
